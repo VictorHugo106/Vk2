03bb23b539be18d0250b61cd29ea8eee42db984e
VictorHugo106 <161029280+VictorHugo106@users.noreply.github.com>
2024-11-22 07:28:10 -0300

    Add files via upload

---
+++ Vk/Vk/templates/Doadores/Doadores.py
@@ -0,0 +1,64 @@
+from flask import Blueprint, render_template, request, redirect, flash
+from models import Doadores
+from database import db
+
+bp_Doadores = Blueprint('Doadores', __name__, template_folder="templates")
+
+@bp_Doadores.route("/")
+def index():
+    p = Doadores.query_all()
+    return render_template("Doadores.html", dados=p)
+
+
+@bp_Doadores.route("/add")
+def add():
+    return render_template("Doadores_add.html")
+
+
+@bp_Doadores.route("/save")
+def save():
+    nome = request.form.get("nome")
+    telefone = request.form.get("telefone")
+    if nome and telefone:
+        db_Doadores = Doadores(nomeDoadoreses, telefone)
+        db.session.add(db_Doadores)
+        db.session.commit()
+        flash("Doadores cadastrado!")
+        return redirect("/Doadores")
+    else:
+        flash("Preencha todos os campos!")
+        return redirect("/Doadores/add")
+    
+
+@bp_Doadores.route("/remove/<int:id>")
+def remove():
+    p = Doadores.query.get(id)
+    try:
+        db.session.delete(p)
+        db.session.commit()
+        flash("Doadores removido!")
+    except:
+        flash("Doadores Inválido!")
+    return redirect("/Doadores")
+
+
+@bp_Doadores.route("/edit/<int:id>")
+def edit():
+    p = Doadores.query.get(id)
+    return render_template("Doadores_edit.html", dados=p)
+
+
+@bp_Doadores.route("/edit-save", methods=['POST'])
+def edit_save():
+    nome = request.form.get("nome")
+    telefone = request.form.get("telefone")
+    id = request.form.get("id")
+    if nome and telefone and id:
+        p = Doadores.query.get(id)
+        p.nome = nome
+        p.telefone = telefone
+        db.session.commit()
+        flash("Dados atualizados!")
+    else:
+        flash("Preencha todos os campos!")
+    return redirect("/Doadores")
